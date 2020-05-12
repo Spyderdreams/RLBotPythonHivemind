2cb7798f02200d56fdbab607c4b02dcbd5f0d3d0
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-05-12 22:06:12 +0200

    Update drone.py

--- src/drone.py
+++ src/drone.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from rlbot.agents.hivemind.drone_agent import DroneAgent
 
-# Dummy agent to call request MyHivemind.
+# Dummy agent to call request the Python Hivemind.
 class Drone(DroneAgent):
     hive_path = str(Path(__file__).parent / "hive.py")
     hive_key = "ChangeThisKeyPlease"
