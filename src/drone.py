763970836a81d63ab996db892e6a5cfe7d880126
Jason McDaniel <spyderdreams@gmail.com>
2025-01-02 02:51:56 -0600

    Update drone.py

--- src/drone.py
+++ src/drone.py
@@ -5,5 +5,5 @@ from rlbot.agents.hivemind.drone_agent import DroneAgent
 # Dummy agent to call request the Python Hivemind.
 class Drone(DroneAgent):
     hive_path = str(Path(__file__).parent / "hive.py")
-    hive_key = "ChangeThisKeyPlease"
-    hive_name = "ExampleHivemind"
+    hive_key = "SpyderHivemindKey123"  # Unique key for your hivemind
+    hive_name = "SpyderHivemind"  # Name for your bot
