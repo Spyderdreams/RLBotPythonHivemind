326767768116f52723151bf482641721f448da2e
Jason McDaniel <spyderdreams@gmail.com>
2025-01-02 02:50:49 -0600

    Update hive.py

--- src/hive.py
+++ src/hive.py
@@ -5,25 +5,47 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 from rlbot.agents.hivemind.python_hivemind import PythonHivemind
 
 
-# Link to Hivemind wiki: https://github.com/ViliamVadocz/Hivemind/wiki/
+class Drone:
+    def __init__(self, index):
+        self.index = index
+        self.position = None
+        self.velocity = None
+        self.role = "idle"
+
 
 class ExampleHivemind(PythonHivemind):
     def initialize_hive(self, packet: GameTickPacket) -> None:
-        self.logger.info("Initialised!")
-
+        self.logger.info("Initialized!")
+        
         # Find out team by looking at packet.
-        # drone_indices is a set, so you cannot just pick first element.
         index = next(iter(self.drone_indices))
         self.team = packet.game_cars[index].team
+        self.logger.info(f"Team {self.team} hivemind initialized with drones: {self.drone_indices}")
 
-        # Initialise objects and attributes here!
-        # I suggest making a Car or Drone object for each of your drones
-        # that will store info about them.
+        # Initialize drones
+        self.drones = {index: Drone(index) for index in self.drone_indices}
 
     def get_outputs(self, packet: GameTickPacket) -> Dict[int, PlayerInput]:
+        outputs = {}
 
-        # Your logic goes here!
+        # Game state analysis
+        ball_position = packet.game_ball.physics.location
 
-        # Return a dictionary where the keys are indices of your drones and
-        # the values are PlayerInput objects (the controller inputs).
-        return {index: PlayerInput(throttle=1.0) for index in self.drone_indices}
+        for index, drone in self.drones.items():
+            # Update drone state
+            drone.position = packet.game_cars[index].physics.location
+            drone.velocity = packet.game_cars[index].physics.velocity
+
+            # Assign roles dynamically
+            if index == list(self.drone_indices)[0]:
+                drone.role = "attacker"
+            else:
+                drone.role = "defender"
+
+            # Role-based logic
+            if drone.role == "attacker":
+                outputs[index] = PlayerInput(throttle=1.0, steer=0.5)  # Example: Move forward and turn
+            elif drone.role == "defender":
+                outputs[index] = PlayerInput(throttle=-1.0)  # Example: Reverse
+
+        return outputs
